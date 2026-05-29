feb72742dd7f9583f1cd49c7a26214ebe0174812
Vaidik118 <vaidikvala10@gmail.com>
2026-05-30 02:04:34 +0530

    Fix NLTK download issue

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import pickle
 import string
-from nltk.corpus import stopwords
 import nltk
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+
+# Download NLTK resources for Streamlit Cloud
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
 
 ps = PorterStemmer()
 
